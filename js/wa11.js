3b37734bbb7d569bb8a7a69863a1aca22adb11c4
jabo2204 <jabo2204@colorado.edu>
2024-11-09 14:37:48 -0700

    please work

--- js/wa11.js
+++ js/wa11.js
@@ -6,16 +6,16 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ['safari_elephant.jpg', 'safari_gazelle.jpg', 'safari_giraffe.jpg', 'safari_lion.jpg', 'safari_zebra.jpg'];
+const images = ['safari_elephant.JPG', 'safari_gazelle.JPG', 'safari_giraffe.JPG', 'safari_lion.JPG', 'safari_zebra.JPG'];
 
 /* Declaring the alternative text for each image file */
 
 const alts = {
-    'safari_elephant.jpg' : 'Closeup of an elephant in the middle of a road.',
-    'safari_gazelle.jpg' : 'Gazelle looking towards camera with wildebeest behind it.',
-    'safari_giraffe.jpg' : 'Grown Giraffe with young giraffe behind it.',
-    'safari_lion.jpg' : 'Lion laying down and partly concealed by bushes.',
-    'safari_zebra.jpg' : 'Two Zebras with giraffe standing behind them.',
+    'safari_elephant.JPG' : 'Closeup of an elephant in the middle of a road.',
+    'safari_gazelle.JPG' : 'Gazelle looking towards camera with wildebeest behind it.',
+    'safari_giraffe.JPG' : 'Grown Giraffe with young giraffe behind it.',
+    'safari_lion.JPG' : 'Lion laying down and partly concealed by bushes.',
+    'safari_zebra.JPG' : 'Two Zebras with giraffe standing behind them.'
 };
 
 /* Looping through images */
